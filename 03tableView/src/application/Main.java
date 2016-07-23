3fac4242aa21c25bc7bcedab52d3dd9e1f48be47
thejavamonk <thejavamonk@gmail.com>
2016-07-23 17:12:45 +0530

    adding table view example

---
+++ 03tableView/src/application/Main.java
@@ -0,0 +1,42 @@
+package application;
+	
+import java.io.IOException;
+
+import controller.MainWindowController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+	private Stage primaryStage;
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		mainWindow();
+	}
+	
+	public void mainWindow(){
+		
+		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindowView.fxml"));
+		try {
+			AnchorPane pane = fxmlLoader.load();
+			MainWindowController controller = fxmlLoader.getController();
+			controller.setMain(this, primaryStage);
+			Scene scene = new Scene(pane);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ 03tableView/src/controller/MainWindowController.java
@@ -0,0 +1,57 @@
+package controller;
+
+import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.Person;
+
+public class MainWindowController {
+
+	private Main main;
+	private Stage primaryStage;
+
+	@FXML
+	private TableView<Person> tableView;
+
+	@FXML
+	private TableColumn<Person, String> firstNameColumn;
+
+	@FXML
+	private TableColumn<Person, String> lastNameColumn;
+
+	@FXML
+	private TableColumn<Person, String> ageColumn;
+
+	private ObservableList<Person> persons = FXCollections.observableArrayList();
+	
+	public void initialize(){
+		firstNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+		lastNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+		ageColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("age"));
+	}
+
+	public void closeButton() {
+		primaryStage.close();
+	}
+	
+	public void populatePerson(){
+		persons.add(new Person("Steve", "Jobs", "78"));
+		persons.add(new Person("Bill", "Gates", "68"));
+		persons.add(new Person("Steve", "Woznaik", "70"));
+	}
+	
+	public void setMain(Main main, Stage primaryStage) {
+		this.main = main;
+		this.primaryStage = primaryStage;
+		populatePerson();
+		tableView.setItems(persons);
+	}
+
+}
